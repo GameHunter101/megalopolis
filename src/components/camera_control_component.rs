59b2fc70e0ea3c0199c388c03be8c205d1b25bf4
Lior Carmeli <liorscarmeli@gmail.com>
2024-09-18 00:36:16 -0400

    Finished all camera controls

--- src/components/camera_control_component.rs
+++ src/components/camera_control_component.rs
@@ -1,14 +1,21 @@
 use algoe::{bivector::Bivector, rotor::Rotor3};
 use gamezap::{ecs::components::transform_component::TransformComponent, new_component};
-use nalgebra::{Matrix4, Vector3, Vector4};
+use nalgebra::Vector3;
 
-new_component!(CameraControlComponent { camera_speed: f32 });
+new_component!(CameraControlComponent {
+    camera_speed: f32,
+    vertical_rotation: Rotor3
+});
 
 impl CameraControlComponent {
     pub fn new(camera_speed: f32) -> Self {
         Self {
             camera_speed,
             parent: EntityId::MAX,
+            vertical_rotation: (algoe::bivector::Bivector::new(0.0, 1.0, 0.0)
+                * -std::f32::consts::FRAC_PI_4
+                / 3.0)
+                .exponentiate(),
             id: (EntityId::MAX, TypeId::of::<Self>(), 0),
         }
     }
@@ -33,7 +40,7 @@ impl ComponentSystem for CameraControlComponent {
         let details = engine_details.lock().unwrap();
         let scancodes = &details.pressed_scancodes;
 
-        let position = *this_concept_manager
+        let mut position = *this_concept_manager
             .get_concept::<Vector3<f32>>(
                 (self.id.0, TypeId::of::<TransformComponent>(), self.id.2),
                 "position".to_string(),
@@ -46,10 +53,10 @@ impl ComponentSystem for CameraControlComponent {
                 if let Some(transform) = comp.as_any_mut().downcast_mut::<TransformComponent>() {
                     transform.apply_rotation(
                         concept_manager.clone(),
-                        (algoe::bivector::Bivector::new(0.0, 1.0, 0.0)
-                            * std::f32::consts::FRAC_PI_4
-                            / 3.0)
-                            .exponentiate(),
+                        Rotor3::new(
+                            self.vertical_rotation.scalar,
+                            self.vertical_rotation.bivector * -1.0,
+                        ),
                     );
 
                     let rotor = (Bivector::new(0.0, 0.0, 1.0) * self.camera_speed).exponentiate();
@@ -60,13 +67,9 @@ impl ComponentSystem for CameraControlComponent {
 
                     transform.apply_translation(concept_manager.clone(), new_position - position);
 
-                    transform.apply_rotation(
-                        concept_manager.clone(),
-                        (algoe::bivector::Bivector::new(0.0, 1.0, 0.0)
-                            * -std::f32::consts::FRAC_PI_4
-                            / 3.0)
-                            .exponentiate(),
-                    );
+                    position = new_position;
+
+                    transform.apply_rotation(concept_manager.clone(), self.vertical_rotation);
                 }
             }
         }
@@ -76,10 +79,10 @@ impl ComponentSystem for CameraControlComponent {
                 if let Some(transform) = comp.as_any_mut().downcast_mut::<TransformComponent>() {
                     transform.apply_rotation(
                         concept_manager.clone(),
-                        (algoe::bivector::Bivector::new(0.0, 1.0, 0.0)
-                            * std::f32::consts::FRAC_PI_4
-                            / 3.0)
-                            .exponentiate(),
+                        Rotor3::new(
+                            self.vertical_rotation.scalar,
+                            self.vertical_rotation.bivector * -1.0,
+                        ),
                     );
 
                     let rotor = (Bivector::new(0.0, 0.0, -1.0) * self.camera_speed).exponentiate();
@@ -90,13 +93,77 @@ impl ComponentSystem for CameraControlComponent {
 
                     transform.apply_translation(concept_manager.clone(), new_position - position);
 
-                    transform.apply_rotation(
-                        concept_manager.clone(),
-                        (algoe::bivector::Bivector::new(0.0, 1.0, 0.0)
-                            * -std::f32::consts::FRAC_PI_4
-                            / 3.0)
-                            .exponentiate(),
-                    );
+                    position = new_position;
+
+                    transform.apply_rotation(concept_manager.clone(), self.vertical_rotation);
+                }
+            }
+        }
+
+        if scancodes.contains(&sdl2::keyboard::Scancode::W) {
+            for comp in component_map.get_mut(&self.parent).unwrap() {
+                if let Some(transform) = comp.as_any_mut().downcast_mut::<TransformComponent>() {
+                    let rotation = (algoe::bivector::Bivector::new(0.0, 1.0, 0.0)
+                        * self.camera_speed)
+                        .exponentiate();
+                    transform.apply_rotation(concept_manager.clone(), rotation);
+
+                    self.vertical_rotation = self.vertical_rotation * rotation;
+                }
+            }
+        }
+
+        if scancodes.contains(&sdl2::keyboard::Scancode::S) {
+            for comp in component_map.get_mut(&self.parent).unwrap() {
+                if let Some(transform) = comp.as_any_mut().downcast_mut::<TransformComponent>() {
+                    let rotation = (algoe::bivector::Bivector::new(0.0, -1.0, 0.0)
+                        * self.camera_speed)
+                        .exponentiate();
+                    transform.apply_rotation(concept_manager.clone(), rotation);
+
+                    self.vertical_rotation = self.vertical_rotation * rotation;
+                }
+            }
+        }
+
+        if scancodes.contains(&sdl2::keyboard::Scancode::A) {
+            for comp in component_map.get_mut(&self.parent).unwrap() {
+                if let Some(transform) = comp.as_any_mut().downcast_mut::<TransformComponent>() {
+                    let new_position = position * (1.0 - self.camera_speed);
+                    transform.apply_translation(concept_manager.clone(), new_position - position);
+
+                    position = new_position;
+                }
+            }
+        }
+
+        if scancodes.contains(&sdl2::keyboard::Scancode::D) {
+            for comp in component_map.get_mut(&self.parent).unwrap() {
+                if let Some(transform) = comp.as_any_mut().downcast_mut::<TransformComponent>() {
+                    let new_position = position * (1.0 + self.camera_speed);
+                    transform.apply_translation(concept_manager.clone(), new_position - position);
+
+                    position = new_position;
+                }
+            }
+        }
+
+        if scancodes.contains(&sdl2::keyboard::Scancode::Space) {
+            for comp in component_map.get_mut(&self.parent).unwrap() {
+                if let Some(transform) = comp.as_any_mut().downcast_mut::<TransformComponent>() {
+                    let new_position = position + Vector3::new(0.0, self.camera_speed, 0.0);
+                    transform.apply_translation(concept_manager.clone(), new_position - position);
+                    position = new_position;
+                }
+            }
+        }
+
+        if scancodes.contains(&sdl2::keyboard::Scancode::LCtrl) {
+            for comp in component_map.get_mut(&self.parent).unwrap() {
+                if let Some(transform) = comp.as_any_mut().downcast_mut::<TransformComponent>() {
+                    let new_position = position - Vector3::new(0.0, self.camera_speed, 0.0);
+                    transform.apply_translation(concept_manager.clone(), new_position - position);
+                    position = new_position;
                 }
             }
         }
